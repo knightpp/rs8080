eabf82814c15bf11d69c12c7a4e35911aa100dd5
Danil <knightpp@pm.me>
2020-04-30 19:12:46 +0300

    newtype cycles struct

--- rs8080-emulator/src/lib.rs
+++ rs8080-emulator/src/lib.rs
@@ -6,3 +6,9 @@ mod traits;
 pub use crate::traits::DataBus;
 pub use structs::RS8080;
 pub use traits::{MemLimiter, WriteAction};
+
+extern crate derive_more;
+use derive_more::{Add, Display, From};
+
+#[derive(Add, Display, From)]
+pub struct ClockCycles(u8);
