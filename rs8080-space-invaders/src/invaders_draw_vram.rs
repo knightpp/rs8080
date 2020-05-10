e72e06003754fac41ae42ef205b1c4ab61bdd822
Danil Kondratiev <knightpp@protonmail.com>
2020-05-10 13:28:56 +0300

    optimize draw allocations

--- rs8080-space-invaders/src/invaders_draw_vram.rs
+++ rs8080-space-invaders/src/invaders_draw_vram.rs
@@ -9,28 +9,27 @@ pub(crate) fn draw_space_invaders_vram(
     screen: &Screen,
 ) -> Result<(), Box<dyn std::error::Error>> {
     assert_eq!(vram.len(), 0x1BFF);
-    let mut slice = [[0u8; 224]; 256];
+    //let mut slice = [[0u8; 224]; 256];
     let mut x = 0usize;
     let mut y = 255usize;
-    for byte in vram.iter() {
-        for pixel in get_bitvec(*byte).iter() {
-            if *pixel {
-                slice[y][x] = screen.white_color;
-            } else {
-                slice[y][x] = screen.black_color;
-            }
-            if y == 0 {
-                x += 1;
-                y = 255;
-            } else {
-                y -= 1;
+    tex.with_lock(None, |buf, _pitch| {
+        for byte in vram.iter() {
+            for pixel in get_bitvec(*byte).iter() {
+                if *pixel {
+                    buf[y * 224 + x] = screen.white_color;
+                } else {
+                    buf[y * 224 + x] = screen.black_color;
+                }
+                if y == 0 {
+                    x += 1;
+                    y = 255;
+                } else {
+                    y -= 1;
+                }
             }
         }
-    }
-    let t: Vec<_> = slice.iter().flat_map(|x| x.to_vec()).collect();
-    tex.with_lock(None, |buf, _pitch| {
-        buf.copy_from_slice(&t);
     })?;
+
     //tex.update(None, &t, 224).unwrap();
     let canvas_size = canvas.window().size();
     let new_width = (screen.width as f64 * canvas_size.1 as f64 / screen.height as f64) as u32;
@@ -47,7 +46,7 @@ pub(crate) fn draw_space_invaders_vram(
     Ok(())
 }
 
-pub(crate) fn get_bitvec(byte: u8) -> [bool; 8] {
+fn get_bitvec(byte: u8) -> [bool; 8] {
     let mut bitvec = [false; 8];
     bitvec[0] = byte & 0b0000_0001 > 0;
     bitvec[1] = byte & 0b0000_0010 > 0;
